b42173fd2481be77a3a249561304b0740a76e359
Curious Emmanuel <107352649+CuriousEmmanuel@users.noreply.github.com>
2024-08-04 10:35:38 +0300

    added js includes images fonts

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,33 +1,31 @@
   <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
+        <div class="container">
 
 
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                  <span class="sr-only">Toggle navigation</span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="index.php">Start Bootstrap</a>
-          </div>
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Start Bootstrap</a>
+            </div>
 
 
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul class="nav navbar-nav">
-                  <?php
-                    //querying and displaying dynamic data from the categories table from the database
-                    $query = "SELECT * FROM categories";
-                    $select_all_categories_query = mysqli_query($connection, $query);
-                    while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
-                        $cat_title = $row["cat_title"];
-                        $cat_id = $row["cat_id"];
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+            <?php
+                //querying and displaying dynamic data from the categories table from the database
+                 $query = "SELECT * FROM categories";
+                 $select_all_categories_query = mysqli_query($connection,$query);
+                 while ($row = mysqli_fetch_assoc($select_all_categories_query)) {
+                     $cat_title = $row["cat_title"];
 
-                        //  add links to the navigation items 
-                        echo "<li><a href='category.php?category={$cat_id}'>{$cat_title}</a></li>";
-                    }
+                     echo "<li><a href='#'>{$cat_title}</a></li>";
+                 }
 
 
             ?>
@@ -37,14 +35,6 @@
                     </li>
                       <li>
                         <a href="registration.php">Registration</a>
-                    ?>
-
-                  <li>
-                      <a href="admin">Admin</a>
-                  </li>
-                  <!-- 
-                    <li>
-                        <a href="#">Services</a>
                     </li>
 
 <?php
